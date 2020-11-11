a0b29801c6c3e3f672f7002e44c35444f03e54e5
sglogowski <s.glogowski@outlook.com>
2020-11-11 12:40:00 +0100

    Bugs/1 index out of range exception when parsing a given polyline (#2)
    * Added unit test from bug.
    Added unit test that base on Google example.
    
    * Fixed bug, added more unit tests for encoding polylines.
    Code refactoring.
    
    * Updated Polyliner.NET.nuspec.
    
    Co-authored-by: Sebastian Głogowski <sebastian.glogowski@rydoo.com>

--- src/Polyliner.NET.Tests/Polyliner_EncodeTests.cs
+++ src/Polyliner.NET.Tests/Polyliner_EncodeTests.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Globalization;
 using Xunit;
 
 namespace PolylinerNet.Tests
@@ -7,17 +6,17 @@ namespace PolylinerNet.Tests
     public class Polyliner_EncodeTests
     {
         [Theory]
-        [InlineData("41.85285", "-87.63941", "ikm~Fha|uO")]     // Chicago
-        [InlineData("52.22778", "20.98614", "svw}Hkza_C")]      // Warsaw
-        [InlineData("41.89997", "12.50083", "yqv~FeqhkA")]      // Rome
-        [InlineData("35.68512", "139.66267", "_wxxEuzlsY")]     // Tokyo
-        [InlineData("-33.45773", "-70.67095", "xuekEl|inL")]    // Sandiego
-        public void Polyliner_EncodeListWithOneElement_ShouldReturnExpectedPolyline(string latitude, string longitude, string expectedPolyline)
+        [InlineData(41.85285, -87.63941, "gkm~Fha|uO")]     // Chicago
+        [InlineData(52.22778, 20.98614, "svw}Hkza_C")]      // Warsaw
+        [InlineData(41.89997, 12.50083, "yqv~FeqhkA")]      // Rome
+        [InlineData(35.68512, 139.66267, "_wxxEuzlsY")]     // Tokyo
+        [InlineData(-33.45773, -70.67095, "xuekEl|inL")]    // Sandiego
+        public void Polyliner_EncodeListWithOneElement_ShouldReturnExpectedPolyline(double latitude, double longitude, string expectedPolyline)
         {
             // arrange
             var polyliner = new Polyliner();
             var points = new List<PolylinePoint> {
-                new PolylinePoint(double.Parse(latitude, CultureInfo.InvariantCulture), double.Parse(longitude, CultureInfo.InvariantCulture))
+                new PolylinePoint(latitude, longitude)
             };
 
             // act
@@ -29,15 +28,37 @@ namespace PolylinerNet.Tests
             Assert.Equal(expectedPolyline, result);
         }
 
-        [Theory]
-        [InlineData("41.86231", "-87.63804", "41.87458", "-87.63460", "mfo~Fvx{uOukAoT")]     // Chicago
-        public void Polyliner_EncodeListWithTwoElements_ShouldReturnExpectedPolyline(string latitude1, string longitude1, string latitude2, string longitude2, string expectedPolyline)
+        // Google exaples(https://developers.google.com/maps/documentation/utilities/polylinealgorithm?csw=1):
+        [Fact]
+        public void Polyliner_EncodeGoogleExamples_ShouldReturnExpectedPolyline()
         {
             // arrange
             var polyliner = new Polyliner();
             var points = new List<PolylinePoint> {
-                new PolylinePoint(double.Parse(latitude1, CultureInfo.InvariantCulture), double.Parse(longitude1, CultureInfo.InvariantCulture)),
-                new PolylinePoint(double.Parse(latitude2, CultureInfo.InvariantCulture), double.Parse(longitude2, CultureInfo.InvariantCulture))
+                new PolylinePoint(38.5, -120.2),
+                new PolylinePoint(40.7, -120.95),
+                new PolylinePoint(43.252, -126.453),
+            };
+
+            // act
+            var result = polyliner.Encode(points);
+
+            // assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+            Assert.Equal(@"_p~iF~ps|U_ulLnnqC_mqNvxq`@", result);
+        }
+
+        [Theory]
+        [InlineData(41.86231, -87.63804, 41.87458, -87.63460, "mfo~Fvx{uOukAoT")]     // Chicago
+        public void Polyliner_EncodeListWithTwoElements_ShouldReturnExpectedPolyline(
+            double latitude1, double longitude1, double latitude2, double longitude2, string expectedPolyline)
+        {
+            // arrange
+            var polyliner = new Polyliner();
+            var points = new List<PolylinePoint> {
+                new PolylinePoint(latitude1, longitude1),
+                new PolylinePoint(latitude2, longitude2)
             };
 
             // act
